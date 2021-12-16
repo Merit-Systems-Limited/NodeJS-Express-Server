34097f1f251c75201310ab3b32abe03c96fd9aa0
Brian Kiprono <briankoech650@gmail.com>
2021-12-16 10:51:07 +0300

    Added the custom 404 error page to the routes

--- server.js
+++ server.js
@@ -9,16 +9,22 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 
-
-server.get('/', (req, res) => {
+server.get('^/$|/index(.html)?', (req, res) => {
     console.log('Welcome Brian Koech');
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-server.get('/new', (req, res) => {
-    console.log('Welcome Brian Koech');
+server.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
 });
 
+// Handling Redirects
+server.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html');
+});
+// Custom 404
+server.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
 server.listen(PORT, () => console.log(`Howdy, you server is running on : http://localhost:${PORT}`));
 
 
