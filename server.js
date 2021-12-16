ea50e947d8416bbc673379c632f96c84b29c8a59
Brian Kiprono <briankoech650@gmail.com>
2021-12-16 09:41:09 +0300

    Getting started with express server

--- server.js
+++ server.js
@@ -1,106 +1,19 @@
-const logEvents = require('./logger');
-const EventEmitter = require('events');
-class Emitter extends EventEmitter {};
-const http = require('http');
+const express  = require("express");
 const path = require('path');
-const fs = require('fs');
-const fsPromises = require('fs').promises;
-
+const cors = require('cors');
 const dotenv = require('dotenv');
-const { contentType } = require('express/lib/response');
-const { response } = require('express');
-const res = require('express/lib/response');
 dotenv.config();
-// Initialize the emitter
-const myEmitter =  new Emitter();
-const port = process.env.PORT || 5222;
-myEmitter.on('log', (msg, fileName) => logEvents(msg, fileName));
+const PORT = process.env.PORT || 5222;
 
+const server = express();
+server.use(express.json());
+server.use(cors());
 
-const serveFile = async (filePath, contentType, response) => {
-    try {
-        const rawData = await fsPromises.readFile(filePath, !contentType.includes('image') ? 'utf-8' : '');
-        const data = contentType === 'application/json' ? JSON.parse(rawData) : rawData;
-        response.writeHead(
-            filePath.includes('404.html') ? 404: 200
-            ,{
-            'Content-Type' : contentType
-            }
-        );
-        response.end(
-            contentType === 'application/json' ? JSON.stringify(data) : data
-        );
-    } catch (error) {
-        console.error(error);
-        myEmitter.emit('log', `${err.name}:${err.message}`, 'errorLog.txt'); 
-        response.statusCode = 500;
-        res.end();
-    }
-}
-
-server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
-    // Emit Event
-    myEmitter.emit('log', `${req.url}\t${req.method}`, 'reqLog.txt'); 
-    const extension = path.extname(req.url)
-    let contentType;
-
-    switch (extension) {
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.js':
-            contentType = 'text/javascript'
-            break;
-        case '.json':
-            contentType = 'application/json'
-            break;
-        case '.jpg':
-            contentType = 'image/jpeg'
-            break;
-        case '.png':
-            contentType = 'image/png'
-            break;
-        case '.txt':
-            contentType = 'tetxt/plain'
-            break;
-        default:
-            contentType = 'text/html';
-            break;
-    }
-
-    let filePath = contentType === 'text/html' && req.url === '/' 
-                    ?  path.join(__dirname, 'views','index.html') 
-                    : contentType === 'text/html' && req.url.slice(-1) === '/' 
-                        ? path.join(__dirname, 'views',req.url,'index.html') 
-                            : contentType === 'text/html'
-                                ? path.join(__dirname, 'views', req.url)
-                                : path.join(__dirname, req.url);
-    // Make .html extension not required in the browser
-    if(!extension && req.url.slice(-1) !== '/') filePath += '.html';
-
-    const fileExists = fs.existsSync(filePath);
-    if (fileExists) {
-        // Serve the file
-        serveFile(filePath, contentType, res);
-    }else{
-        switch (path.parse(filePath)) {
-            case 'old-page.html':
-                res.writeHead(301, {'location' : '/new-page.html'});
-                res.end();
-                break;
-            case 'www-page.html':
-                res.writeHead(301, {'location' : '/'});
-                res.end();
-                break;
-            default:
-                //Serve a 404 response
-                serveFile(path.join(__dirname, 'views','404.html'), 'text/html', res);
-                break;
-        }
-    }
+server.get('/', () => {
+    console.log('Welcome Brian Koech');
 });
 
-server.listen(port, () => {
-    console.log(`Server running on port : http://localhost:${port}`);
-});
+server.listen(PORT, () => console.log(`Howdy, you server is running on : http://localhost:${PORT}`));
+
+
+module.exports.server = server;
